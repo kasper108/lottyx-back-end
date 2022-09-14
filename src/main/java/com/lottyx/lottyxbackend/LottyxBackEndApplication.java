a0fa64464522e68e2736a7df4005da1f75f60f67
Stanislovas Kasperiūnas <stanislovaskasperiunas@gmail.com>
2022-09-14 06:00:29 +0300

    Initial commit

---
+++ src/main/java/com/lottyx/lottyxbackend/LottyxBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.lottyx.lottyxbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LottyxBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LottyxBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lottyx/lottyxbackend/LottyxBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lottyx.lottyxbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LottyxBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
